ce0f30970a824c173e7cb3d2b14c11c6bda4b3e6
小白-白 <xb@fczbl.vip>
2017-12-20 14:03:41 +0800

    Update comments.php

--- comments.php
+++ comments.php
@@ -46,4 +46,47 @@ if ( post_password_required() ) {
 		);
 		comment_form($args);
 	?>
+	<script type="text/javascript" src="/wp-content/themes/Kratos/js/OwO.min.js"></script>
+	<script>
+	var OwO_demo = new OwO({
+		logo: 'OωO表情',
+		container: document.getElementsByClassName('OwO')[0],
+		target: document.getElementsByClassName('OwO')[0],
+		api: '/wp-content/themes/kratos/inc/OwO.json',
+		position: 'down',
+		width: '90%',
+		maxHeight: '250px'
+	});
+	function grin(tag) {
+		var myField;
+		tag = ' ' + tag + ' ';
+		if (document.getElementById('comment') && document.getElementById('comment').type == 'textarea') {
+		myField = document.getElementById('comment');
+		} else {
+			return false;
+		}
+		if (document.selection) {
+			myField.focus();
+		sel = document.selection.createRange();
+		sel.text = tag;
+		myField.focus();
+		} else if (myField.selectionStart || myField.selectionStart == '0') {
+			var startPos = myField.selectionStart;
+			var endPos = myField.selectionEnd;
+			var cursorPos = endPos;
+			myField.value = myField.value.substring(0, startPos)
+				+ tag
+				+ myField.value.substring(endPos, myField.value.length);
+			cursorPos += tag.length;
+			myField.focus();
+			myField.selectionStart = cursorPos;
+			myField.selectionEnd = cursorPos;
+			var owoopen = document.getElementsByClassName('OwO OwO-open')[0];
+			owoopen.className = "OwO";
+		} else {
+			myField.value += tag;
+			myField.focus();
+		}
+	}
+</script>
 </div>
