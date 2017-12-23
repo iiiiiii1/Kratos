23982c9b2b39e07a71fe7aeb7dad9fa5bfeccb3b
小白-白 <xb@fczbl.vip>
2017-12-23 11:52:53 +0800

    为雪花插件增加更多设置

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
  * The template for displaying the footer
  *
  * @package Vtrois
- * @version 2.5(17.12.20)
+ * @version 2.5(17.12.23)
  */
 ?>
 				<footer>
@@ -63,114 +63,138 @@
 		</script>
 		<?php endif; ?>
 		<script type="text/javascript"><?php if(is_home()) echo 'var isindex=true;var title="";';else echo 'var isindex=false;var title="', get_the_title(),'";'; ?></script>
+		<?php if ( kratos_option('site_spig') ) : ?>
 		<div id="spig" class="spig">
 			<div id="message">Loading……</div>
 			<div id="mumu" class="mumu"></div>
 			<span class="hitokoto" id="hitokoto" style="display:none">Loading...</span>
 			<div id="hjsbox" style="display:none"></div>
+			<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri();?>/js/spig.js"></script>
 		</div>
-		<?php if ( kratos_option('site_snow')==1 ) : ?>
+		<?php endif; ?>
+		<?php if ( kratos_option('site_snow') ) : ?>
+		<div id="snow_xb" class="snow_xb">
 		<canvas id="Snow"></canvas>
 		<script>
-    (function() {
-        var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
-        function(callback) {
-            window.setTimeout(callback, 1000 / 60);
-        };
-        window.requestAnimationFrame = requestAnimationFrame;
-    })();
-    (function() {
-        var flakes = [],
-            canvas = document.getElementById("Snow"),
-            ctx = canvas.getContext("2d"),
-            flakeCount = 300,
-            mX = -100,
-            mY = -100
-            canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        function snow() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            for (var i = 0; i < flakeCount; i++) {
-                var flake = flakes[i],
-                    x = mX,
-                    y = mY,
-                    minDist = 150,
-                    x2 = flake.x,
-                    y2 = flake.y;
-                var dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
-                    dx = x2 - x,
-                    dy = y2 - y;
-                if (dist < minDist) {
-                    var force = minDist / (dist * dist),
-                        xcomp = (x - x2) / dist,
-                        ycomp = (y - y2) / dist,
-                        deltaV = force / 2;
-                    flake.velX -= deltaV * xcomp;
-                    flake.velY -= deltaV * ycomp;
-                } else {
-                    flake.velX *= 0.98;
-                    if (flake.velY <= flake.speed) {
-                        flake.velY = flake.speed
-                    }
-                    flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
-                }
-                ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
-                flake.y += flake.velY;
-                flake.x += flake.velX;
-                if (flake.y >= canvas.height || flake.y <= 0) {
-                    reset(flake);
-                }
-                if (flake.x >= canvas.width || flake.x <= 0) {
-                    reset(flake);
-                }
-                ctx.beginPath();
-                ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
-                ctx.fill();
-            }
-            requestAnimationFrame(snow);
-        };
-        function reset(flake) {
-            flake.x = Math.floor(Math.random() * canvas.width);
-            flake.y = 0;
-            flake.size = (Math.random() * 3) + 2;
-            flake.speed = (Math.random() * 1) + 0.5;
-            flake.velY = flake.speed;
-            flake.velX = 0;
-            flake.opacity = (Math.random() * 0.5) + 0.3;
-        }
-        function init() {
-            for (var i = 0; i < flakeCount; i++) {
-                var x = Math.floor(Math.random() * canvas.width),
-                    y = Math.floor(Math.random() * canvas.height),
-                    size = (Math.random() * 3) + 2,
-                    speed = (Math.random() * 1) + 0.5,
-                    opacity = (Math.random() * 0.5) + 0.3;
-                flakes.push({
-                    speed: speed,
-                    velY: speed,
-                    velX: 0,
-                    x: x,
-                    y: y,
-                    size: size,
-                    stepSize: (Math.random()) / 30,
-                    step: 0,
-                    angle: 180,
-                    opacity: opacity
-                });
-            }
-            snow();
-        };
-        document.addEventListener("mousemove", function(e) {
-            mX = e.clientX,
-            mY = e.clientY
-        });
-        window.addEventListener("resize", function() {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-        });
-        init();
-    })();
-        </script>
+		(function() {
+			var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
+			function(callback) {
+				window.setTimeout(callback, 1000 / 60);
+			};
+			window.requestAnimationFrame = requestAnimationFrame;
+			})();
+			(function() {
+			var flakes = [],
+				canvas = document.getElementById("Snow"),
+				ctx = canvas.getContext("2d"),
+				flakeCount = <?php echo kratos_option('snow_xb2016_flakecount');?>,
+				mX = -100,
+				mY = -100
+				canvas.width = window.innerWidth;
+				canvas.height = window.innerHeight;
+				function snow() {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				for (var i = 0; i < flakeCount; i++) {
+					var flake = flakes[i],
+					x = mX,
+				y = mY,
+					minDist = <?php echo kratos_option('snow_xb2016_mindist');?>,
+					x2 = flake.x,
+					y2 = flake.y;
+					var dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
+					dx = x2 - x,
+					dy = y2 - y;
+					if (dist < minDist) {
+						var force = minDist / (dist * dist),
+						xcomp = (x - x2) / dist,
+						ycomp = (y - y2) / dist,
+						deltaV = force / 2;
+						flake.velX -= deltaV * xcomp;
+						flake.velY -= deltaV * ycomp;
+					} else {
+						flake.velX *= 0.98;
+						if (flake.velY <= flake.speed) {
+							flake.velY = flake.speed
+						}
+					flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
+					}
+				ctx.fillStyle = "rgba(<?php echo kratos_option('snow_xb2016_snowcolor');?>," + flake.opacity + ")";
+				flake.y += flake.velY;
+				flake.x += flake.velX;
+					if (flake.y >= canvas.height || flake.y <= 0) {
+					reset(flake);
+					}
+					if (flake.x >= canvas.width || flake.x <= 0) {
+					reset(flake);
+					}
+					ctx.beginPath();
+					ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
+					ctx.fill();
+				}
+				requestAnimationFrame(snow);
+			};
+			function reset(flake) {
+				flake.x = Math.floor(Math.random() * canvas.width);
+				flake.y = 0;
+				flake.size = (Math.random() * 3) + 2;
+				flake.speed = (Math.random() * 1) + 0.5;
+				flake.velY = flake.speed;
+				flake.velX = 0;
+				flake.opacity = (Math.random() * 0.5) + 0.3;
+			}
+			function init() {
+				for (var i = 0; i < flakeCount; i++) {
+					var x = Math.floor(Math.random() * canvas.width),
+						y = Math.floor(Math.random() * canvas.height),
+						size = (Math.random() * 3) + <?php echo kratos_option('snow_xb2016_size');?>,
+						speed = (Math.random() * 1) + <?php echo kratos_option('snow_xb2016_speed');?>,
+						opacity = (Math.random() * 0.5) + <?php echo kratos_option('snow_xb2016_opacity');?>;
+					flakes.push({
+						speed: speed,
+						velY: speed,
+						velX: 0,
+						x: x,
+						y: y,
+						size: size,
+						stepSize: (Math.random()) / 30 * <?php echo kratos_option('snow_xb2016_stepsize');?>,
+						step: 0,
+						angle: 180,
+						opacity: opacity
+					});
+				}
+				snow();
+			};
+			document.addEventListener("mousemove", function(e) {
+				mX = e.clientX,
+				mY = e.clientY
+			});
+			window.addEventListener("resize", function() {
+				canvas.width = window.innerWidth;
+				canvas.height = window.innerHeight;
+			});
+		init();
+    	})();
+		</script>
+		<style>
+		#Snow {
+			position:fixed;
+			top:0;
+			left:0;
+			width:100%;
+			height:100%;
+			z-index:99999;
+			background:RGB(<?php echo kratos_option('snow_xb2016_bgcolor');?>);
+			pointer-events:none
+		}
+		<?php if ( kratos_option('snow_xb2016_mobile')!=1 ) : ?>
+		@media screen and (max-width:535px) {
+			#Snow {
+				display:none
+			}
+		}
+		<?php endif; ?>
+		</style>
+		</div>
 		<?php endif; ?>
 	</body>
 </html>
